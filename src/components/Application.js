8350702cdec98fe64afe1b6a2bc5ffa056a74fae
Tim Kopplin <timkopplin@hotmail.com>
2022-11-29 16:28:10 -0800

    day list integrated

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,28 @@ import React from "react";
 
 import "components/Application.scss";
 
+import DayList from "./DayList";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+
+
 export default function Application(props) {
   return (
     <main className="layout">
@@ -12,7 +34,13 @@ export default function Application(props) {
           alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu"></nav>
+        <nav className="sidebar__menu">
+        <DayList
+          days={days}
+          day={"Monday"}
+          setDay={day => console.log(day)}
+        />
+        </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
@@ -20,7 +48,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {/* Replace this with the schedule elements during the "The Scheduler" activity. */}
       </section>
     </main>
   );
