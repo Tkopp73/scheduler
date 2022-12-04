154d87ae2eaf91192d1fb7c4360a6813e6fde100
Tim Kopplin <timkopplin@hotmail.com>
2022-12-03 19:32:07 -0800

    adding prettier

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,74 +1,66 @@
-
-
 const getAppointmentsForDay = (state, day) => {
-  let appointments = [];
-  let appointmentsForDay = [];
+	let appointments = [];
+	let appointmentsForDay = [];
 
-  if (state.appointments) {
-    let appointmentObj = Object.values(state.appointments);
-  
-    for (let theDay of state.days) {
-      if (day === theDay.name) {
-        appointments.push(theDay.appointments);
-      }
-    }
-  
-    for (let appointment of appointments) {
-      for ( let apptObj of appointmentObj) {
-        appointment.forEach((appt)=> {
-          if (appt === apptObj.id) {
-            appointmentsForDay.push(apptObj);
-          }
-      })
-      }
-    }
-  }
+	if (state.appointments) {
+		let appointmentObj = Object.values(state.appointments);
 
-return appointmentsForDay;
-}
+		for (let theDay of state.days) {
+			if (day === theDay.name) {
+				appointments.push(theDay.appointments);
+			}
+		}
 
+		for (let appointment of appointments) {
+			for (let apptObj of appointmentObj) {
+				appointment.forEach((appt) => {
+					if (appt === apptObj.id) {
+						appointmentsForDay.push(apptObj);
+					}
+				});
+			}
+		}
+	}
 
+	return appointmentsForDay;
+};
 
 const getInterviewersForDay = (state, day) => {
-  let appointments = [];
-  let appointmentsForDay = [];
+	let appointments = [];
+	let appointmentsForDay = [];
 
-  if (state.appointments) {
-    let appointmentObj = Object.values(state.appointments);
-  
-    for (let theDay of state.days) {
-      if (day === theDay.name) {
-        appointments.push(theDay.appointments);
-      }
-    }
-  
-    for (let appointment of appointments) {
-      for ( let apptObj of appointmentObj) {
-        appointment.forEach((appt)=> {
-          if (appt === apptObj.id) {
-            appointmentsForDay.push(apptObj);
-          }
-      })
-      }
-    }
-  }
+	if (state.appointments) {
+		let appointmentObj = Object.values(state.appointments);
 
-  return appointmentsForDay;
+		for (let theDay of state.days) {
+			if (day === theDay.name) {
+				appointments.push(theDay.appointments);
+			}
+		}
 
-}
+		for (let appointment of appointments) {
+			for (let apptObj of appointmentObj) {
+				appointment.forEach((appt) => {
+					if (appt === apptObj.id) {
+						appointmentsForDay.push(apptObj);
+					}
+				});
+			}
+		}
+	}
 
+	return appointmentsForDay;
+};
 
 const getInterview = (state, interview) => {
+	if (interview) {
+		return {
+			student: interview.student,
+			interviewer: state.interviewers[interview.interviewer],
+		};
+	} else {
+		return interview;
+	}
+};
 
-  if(interview) {
-    return {'student': interview.student, "interviewer": state.interviewers[interview.interviewer]}
-  } else {
-    return interview;
-  }
-
-}
-
-
-
-
-export { getAppointmentsForDay, getInterview, getInterviewersForDay, };
+export { getAppointmentsForDay, getInterview, getInterviewersForDay };
